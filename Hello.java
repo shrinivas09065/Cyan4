6ed7b8204fe208fd61a268355a63fd3783414b43
shrinivas09065 <shrinivas.wable_24uds@sanjivani.edu.in>
2024-12-26 22:19:28 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,5 @@
 class Hello{
   public static void main(String[] args){
-    System.out.println("Hello cyancut sir");
+    System.out.println("Hello cyancut sir . the best valorant player than jonathan!!");
   }
 }
